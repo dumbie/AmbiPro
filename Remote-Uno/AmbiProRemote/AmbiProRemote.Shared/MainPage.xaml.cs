cfc02aa1e5b0e27c5411062a873b1c448b7dc7e4
Arnold Vink <info@arnoldvink.com>
2022-01-05 17:28:58 +0100

    - Added popups to the mainpage.

--- Remote-Uno/AmbiProRemote/AmbiProRemote.Shared/MainPage.xaml.cs
+++ Remote-Uno/AmbiProRemote/AmbiProRemote.Shared/MainPage.xaml.cs
@@ -1,17 +1,81 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace AmbiProRemote
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                ColorPickerLoad();
+                //Fix: Check if server ip and port are set open settings otherwise
+            }
+            catch { }
+        }
+
+        void ClosePopups()
+        {
+            try
+            {
+                popup_ColorPicker.Visibility = Visibility.Collapsed;
+                popup_Help.Visibility = Visibility.Collapsed;
+                popup_Settings.Visibility = Visibility.Collapsed;
+            }
+            catch { }
+        }
+
+        private void Button_ClosePopups_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ClosePopups();
+            } 
+            catch { }
+        }
+
+        private async void Button_LedsOnOffSwitch(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await SocketSend.SocketSendAmbiPro("LedSwitch");
+            }
+            catch { }
+        }
+
+        private void Button_ShowColorPicker_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                popup_ColorPicker.Visibility = Visibility.Visible;
+            }
+            catch { }
+        }
+
+        private void Button_ShowHelp_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                popup_Help.Visibility = Visibility.Visible;
+            }
+            catch { }
+        }
+
+        private void Button_ShowSettings_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                popup_Settings.Visibility = Visibility.Visible;
+            }
+            catch { }
         }
     }
 }
