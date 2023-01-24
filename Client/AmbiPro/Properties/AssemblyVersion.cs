3ff663f205c7f4ac48ed8ee3a146efcde464b168
Arnold Vink <info@arnoldvink.com>
2023-01-24 23:04:44 +0100

    - Updated to latest AVCode changes.

--- Client/AmbiPro/Properties/AssemblyVersion.cs
+++ Client/AmbiPro/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly 
-[assembly: AssemblyVersion("0.22.0.0")]
-[assembly: AssemblyFileVersion("0.22.0.0")]
+[assembly: AssemblyVersion("0.23.0.0")]
+[assembly: AssemblyFileVersion("0.23.0.0")]
