a01cbb4de4fa13c90d3a9054446aacc3b3d9071b
Arnold Vink <info@arnoldvink.com>
2025-02-04 10:30:24 +0100

    - Updated application NuGet packages. - Updated version number.

--- Client/AmbiPro/Properties/AssemblyVersion.cs
+++ Client/AmbiPro/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly 
-[assembly: AssemblyVersion("0.26.1.0")]
-[assembly: AssemblyFileVersion("0.26.1.0")]
+[assembly: AssemblyVersion("0.27.0.0")]
+[assembly: AssemblyFileVersion("0.27.0.0")]
